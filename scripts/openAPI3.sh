fbc908afc5e7c1679900efb76fe2eba87163f45e
Denis Yuen <denis.yuen@oicr.on.ca>
2019-06-12 14:22:44 -0400

    More convention sync with cloud workstream APIs (#84)
    * Working on #82 also see https://github.com/ga4gh/workflow-execution-service-schemas/issues/134
    * Working on harmonizing with GA4GH Discovery  https://github.com/ga4gh-discovery/service-info
    * Fix description for #85

--- scripts/openAPI3.sh
+++ scripts/openAPI3.sh
@@ -4,14 +4,14 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-if ! git diff --exit-code src/main/resources/swagger/openapi.yaml
+if ! git diff --exit-code openapi/openapi.yaml
   then
-    git diff src/main/resources/swagger/openapi.yaml
+    git diff openapi/openapi.yaml
     openssl aes-256-cbc -K $encrypted_f356a5c69655_key -iv $encrypted_f356a5c69655_iv -in github_deploy_key.enc -out github_deploy_key -d
     eval "$(ssh-agent)"
     chmod 600 github_deploy_key
     ssh-add github_deploy_key
-    git add src/main/resources/swagger/openapi.yaml
+    git add openapi/openapi.yaml
     git commit -m "OpenAPI changed"
     echo "Pushing changes"
     git push git@github.com:"${TRAVIS_REPO_SLUG}" ${TRAVIS_BRANCH}
