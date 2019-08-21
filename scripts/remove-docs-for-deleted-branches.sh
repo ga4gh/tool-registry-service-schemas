955d91bee49c7823528514a356fddf1a7e2ac8db
Gary Luu <garylau.work@gmail.com>
2019-08-21 17:35:53 -0400

    Handle tags (#111)

--- scripts/remove-docs-for-deleted-branches.sh
+++ scripts/remove-docs-for-deleted-branches.sh
@@ -9,7 +9,17 @@ for folder in preview/*; do
         # Only get the directory name, not anything before it
         folder2=${folder##*/}
         # If there isn't a git branch with that name, delete it 
-        if ! git branch -r --sort=-committerdate | cut -d "/" -f 2 | grep -q "${folder2}"; then
+        if git branch -r | cut -d "/" -f 2 | grep -q ${folder2} ; then
+	  IS_BRANCH=true
+	else 
+          IS_BRANCH=false
+	fi
+	if git tag | grep -q ${folder2} ; then
+	  IS_TAG=true
+	else
+          IS_TAG=false
+	fi
+        if ! ${IS_BRANCH} && ! ${IS_TAG} ; then
                 rm -rf "${folder}"
         fi
 done
