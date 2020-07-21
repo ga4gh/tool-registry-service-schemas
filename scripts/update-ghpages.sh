50ed9ad765edb5f6841b1660b311449e5a1546cf
garyluu <garylau.work@gmail.com>
2020-07-20 20:00:36 -0400

    Fix script

--- scripts/update-ghpages.sh
+++ scripts/update-ghpages.sh
@@ -4,13 +4,14 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 set -u
-BRANCH=$(echo "$TRAVIS_BRANCH")
+BRANCH=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
 BRANCH_PATH="preview/$BRANCH"
 mv preview preview2
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch --deepen=50000
+git checkout gh-pages
 rm -rf preview
-cp preview2 preview
+mv preview2 preview
 git status
 if [[ -n "$(git status --porcelain "${BRANCH_PATH}")" && ${TRAVIS_PULL_REQUEST} == "false" ]]; then
   openssl aes-256-cbc -K $encrypted_f356a5c69655_key -iv $encrypted_f356a5c69655_iv -in github_deploy_key.enc -out github_deploy_key -d
