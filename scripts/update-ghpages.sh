f6cd75ccd7d411bb5ac3acb0e5f0db5622e18dd5
Denis Yuen <denis.yuen@oicr.on.ca>
2021-08-25 11:21:51 -0400

    test

--- scripts/update-ghpages.sh
+++ scripts/update-ghpages.sh
@@ -4,7 +4,7 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 set -u
-BRANCH=$(echo "$TRAVIS_BRANCH" | awk '{print tolower($0)}')
+BRANCH=$(echo "$GITHUB_REF" | awk '{print tolower($0)}')
 BRANCH_PATH="preview/$BRANCH"
 mv preview preview2
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
@@ -13,17 +13,14 @@ git checkout gh-pages
 rm -rf preview
 mv preview2 preview
 git status
-if [[ -n "$(git status --porcelain "${BRANCH_PATH}")" && ${TRAVIS_PULL_REQUEST} == "false" ]]; then
-  openssl aes-256-cbc -K $encrypted_f356a5c69655_key -iv $encrypted_f356a5c69655_iv -in github_deploy_key.enc -out github_deploy_key -d
-  eval "$(ssh-agent)"
-  chmod 600 github_deploy_key
-  ssh-add github_deploy_key
+# looks like github automatically does not make secrets available to forks https://docs.github.com/en/actions/reference/encrypted-secrets
+if [[ -n "$(git status --porcelain "${BRANCH_PATH}")" ]]; then
   bash scripts/create-table-of-contents.sh
   bash scripts/remove-docs-for-deleted-branches.sh
   git add preview
   git add TableOfContents.md
-  git commit -m "Docs changed for "${TRAVIS_BRANCH}""
-  git push git@github.com:"${TRAVIS_REPO_SLUG}" gh-pages
+  git commit -m "Docs changed for "${GITHUB_REF}""
+  git push git@github.com:"${GITHUB_REPOSITORY}" gh-pages
 else
   echo "No changes"
 fi
