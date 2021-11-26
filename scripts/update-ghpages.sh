be6fccd9194486a24aa47af386812ae9a8056bbc
Denis Yuen <denis.yuen@oicr.on.ca>
2021-11-26 11:11:29 -0500

    Update update-ghpages.sh

--- scripts/update-ghpages.sh
+++ scripts/update-ghpages.sh
@@ -22,7 +22,7 @@ if [[ -n "$(git status --porcelain "${BRANCH_PATH}")" ]]; then
   git add preview
   git add TableOfContents.md
   git commit -m "Docs changed for "${GITHUB_REF##*/}""
-  git remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/$GITHUB_REPOSITORY
+  git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/$GITHUB_REPOSITORY
   git push git@github.com:"${GITHUB_REPOSITORY}" gh-pages
 else
   echo "No changes"
