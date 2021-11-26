6e5d3038623b7ba9937ddb6e622b46afb735459a
Denis Yuen <denis.yuen@oicr.on.ca>
2021-11-26 13:10:17 -0500

    Fix up github action token passing (#209)
    * Update update-ghpages.sh

--- scripts/update-ghpages.sh
+++ scripts/update-ghpages.sh
@@ -15,12 +15,15 @@ mv preview2 preview
 git status
 # looks like github automatically does not make secrets available to forks https://docs.github.com/en/actions/reference/encrypted-secrets
 if [[ -n "$(git status --porcelain "${BRANCH_PATH}")" ]]; then
+  git config --global user.email "theglobalalliance@genomicsandhealth.org"
+  git config --global user.name "Automatic documentation build"
   bash scripts/create-table-of-contents.sh
   bash scripts/remove-docs-for-deleted-branches.sh
   git add preview
   git add TableOfContents.md
   git commit -m "Docs changed for "${GITHUB_REF##*/}""
-  git push git@github.com:"${GITHUB_REPOSITORY}" gh-pages
+  git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/$GITHUB_REPOSITORY
+  git push origin gh-pages
 else
   echo "No changes"
 fi
